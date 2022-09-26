005e7e84909b4699195d1f2129ddcc9530a07315
odaadvanced <stbaoda@gmail.com>
2022-09-26 14:09:52 -0500

    class files

---
+++ delmar/03_05_double_dice_while_break.py
@@ -0,0 +1,13 @@
+#03_05_double_dice_while_break
+import random
+count = 1
+while True:
+	throw_1 = random.randint(1, 6)
+	throw_2 = random.randint(1, 6)
+	total = throw_1 + throw_2
+	count = count + 1
+	print(total)
+	if throw_1 == 6 and throw_2 == 6:
+		break
+print (count)
+print('Double Six thrown!')
